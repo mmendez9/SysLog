1f0858c6980de0b9bcd86649b7637c5cd7bcf5c6
student <mmendez9@dtcc.edu>
2017-10-31 10:28:06 -0400

    Regex search

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-#!/bin/python
-
 import os
+import re
 
+# Prompt user to enter file's location and name
+Loc = input("Enter the file's location(Path):".strip())
+os.chdir(Loc)
+file = input("Enter the file's name:".strip())
+
+# Open files
+sysFile = open(file, "r")
+output = open("find.txt", "w")
+
+# Prompt user what to search
+search = input("Enter what to search:".strip())
+
+# Read the file
+for line in sysFile:
+    line = line.rstrip()
+    if re.search(search, line):
+        output.write(line)
